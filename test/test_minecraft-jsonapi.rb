cd44da8245c909d630105c4b042c14fd81105d25
Steven Hoffman <git@fustrate.com>
2013-01-18 14:04:37 -0800

    Tests!
    Signed-off-by: Steven Hoffman <git@fustrate.com>

---
+++ test/test_minecraft-jsonapi.rb
@@ -0,0 +1,27 @@
+require 'test/unit'
+require 'minecraft-jsonapi'
+
+class MinecraftJSONAPITest < Test::Unit::TestCase
+	def test_create_new_shortcut
+		assert_equal Minecraft::JSONAPI::JSONAPI, Minecraft::JSONAPI.new(username: "admin", password: "12345", salt: "mmm").class
+	end
+
+	def test_map_to_array
+		assert_equal [], Minecraft::JSONAPI.map_to_array(nil)
+		assert_equal [""], Minecraft::JSONAPI.map_to_array("")
+		assert_equal ["Fustrate"], Minecraft::JSONAPI.map_to_array("Fustrate")
+
+		assert_equal [12345], Minecraft::JSONAPI.map_to_array(12345)
+		assert_equal [1, 2, 3], Minecraft::JSONAPI.map_to_array([1, 2, 3])
+		assert_equal [1, "2", 3.0], Minecraft::JSONAPI.map_to_array([1, "2", 3.0])
+
+		hash = { :one => "two", :three => "four" }
+
+		assert_equal hash, Minecraft::JSONAPI.map_to_array(hash)
+	end
+
+	def test_url_encoded_json
+		assert_equal "%5B%22test%22%5D", Minecraft::JSONAPI.url_encoded_json(["test"])
+		assert_equal "%5B%22test%22%2C5%5D", Minecraft::JSONAPI.url_encoded_json(["test", 5])
+	end
+end
