03b38b844f8a9c167862f10a188e44e62f8c253b
Steven Hoffman <git@fustrate.com>
2013-01-18 15:41:14 -0800

    Version 0.0.7
    No need for nested namespaces, at least not yet
    
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/minecraft-jsonapi/namespace.rb
+++ lib/minecraft-jsonapi/namespace.rb
@@ -10,15 +10,11 @@ module Minecraft
 				[parent.namespace, @namespace].reject {|x| x.nil? }.join(".")
 			end
 
-			def method_missing(method, *args, &block)
-				if block_given?
-					block.call Minecraft::JSONAPI::Namespace.new(self, method.to_s)
-				else
-					method = [namespace, method.to_s].join(".")
-					url = parent.make_url(method, args)
+			def method_missing(method, *args)
+				method = [namespace, method.to_s].join(".")
+				url = parent.make_url(method, args)
 
-					Minecraft::JSONAPI.send_request(url)
-				end
+				Minecraft::JSONAPI.send_request(url)
 			end
 
 			# This will bubble upwards until we hit the actual JSONAPI class
--- lib/minecraft-jsonapi/version.rb
+++ lib/minecraft-jsonapi/version.rb
@@ -1,5 +1,5 @@
 module Minecraft
 	module JSONAPI
-		VERSION = "0.0.6"
+		VERSION = "0.0.7"
 	end
 end
